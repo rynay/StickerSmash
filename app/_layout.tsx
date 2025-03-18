6991a7e86fb9e67cdfde3c86ffccba58e5fc02ad
Kseniia <feirs911@gmail.com>
2025-03-18 22:41:22 +0300

    add appbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from 'expo-router'
+import { StatusBar } from 'react-native'
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar barStyle="light-content" />
+    </>
   )
 }
