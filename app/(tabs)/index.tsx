e4d480847f27fe5d26b6bfca3ec1c8ed91ea7f0a
Kseniia <feirs911@gmail.com>
2025-03-18 22:35:03 +0300

    save in browser

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,11 @@
+import { StyleSheet, View, Platform } from 'react-native'
 import { useRef, useState } from 'react'
 import * as MediaLibrary from 'expo-media-library'
 import { captureRef } from 'react-native-view-shot'
-import { StyleSheet, View } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import domtoimage from 'dom-to-image'
+
 import ImageViewer from '@/components/ImageViewer'
 import IconButton from '@/components/IconButton'
 import CircleButton from '@/components/CircleButton'
@@ -11,7 +14,6 @@ import EmojiPicker from '@/components/EmojiPicker'
 import EmojiList from '@/components/EmojiList'
 import { ImageSource } from 'expo-image'
 import EmojiSticker from '@/components/EmojiSticker'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 const ImagePlaceholder = require('@/assets/images/background-image.png')
 
@@ -58,18 +60,35 @@ export default function Index() {
   }
 
   const onSaveImage = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      })
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        })
 
-      await MediaLibrary.saveToLibraryAsync(localUri)
-      if (localUri) {
-        alert('Saved!')
+        await MediaLibrary.saveToLibraryAsync(localUri)
+        if (localUri) {
+          alert('Saved!')
+        }
+      } catch (e) {
+        console.log(e)
       }
-    } catch (e) {
-      console.log(e)
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        })
+
+        let link = document.createElement('a')
+        link.download = 'sticker-smash.jpeg'
+        link.href = dataUrl
+        link.click()
+      } catch (e) {
+        console.log(e)
+      }
     }
   }
 
